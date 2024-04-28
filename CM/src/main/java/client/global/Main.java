c9dad4160b20e8ad6625834dbe581dbbab12aad3
DingWoonee <slm0523@naver.com>
2024-04-29 07:51:43 +0900

    client broadcast 클래스를 따로 만듦

--- CM/src/main/java/client/global/Main.java
+++ CM/src/main/java/client/global/Main.java
@@ -34,34 +34,4 @@ public class Main {
 		// set mainFrame in ClientEventHandler
 		cmClientApp.getCmClientEventHandler().setMainFrame(mainFrame);
 	}
-
-	public static void broadcastShape(GShape gShape) {
-		String shapeDetails = Tools.serializeShape(gShape);
-
-		broadcacstMessage("ADD", shapeDetails);
-	}
-
-	public static void broadcastUpdate(GShape gShape) {
-		String shapeDetails = Tools.serializeShape(gShape);
-
-		broadcacstMessage("UPD", shapeDetails);
-	}
-
-	public static void broadcastDelete(GShape gShape) {
-		String shapeDetails = Tools.serializeShape(gShape);
-
-		broadcacstMessage("DEL", shapeDetails);
-	}
-
-	private static void broadcacstMessage(String code, String message) {
-		CMInteractionInfo interInfo = Main.cmClientApp.getCmClientStub().getCMInfo().getInteractionInfo();
-		CMUser myself = interInfo.getMyself();
-
-		CMDummyEvent due = new CMDummyEvent();
-		due.setHandlerSession(myself.getCurrentSession());
-		due.setHandlerGroup(myself.getCurrentGroup());
-		due.setDummyInfo(code + message);
-
-		Main.cmClientApp.getCmClientStub().broadcast(due);
-	}
 }
